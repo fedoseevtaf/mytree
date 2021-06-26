274d64164fd22141a94f935747b9b7a0b67000a0
fedoseevtaf <76451152+fedoseevtaf@users.noreply.github.com>
2021-06-27 00:00:34 +0300

    refactoring
    I :
    * Add type hinting.
    * Rename constants as in PEP.
    * :)

--- tree.py
+++ tree.py
@@ -1,64 +1,61 @@
-#==============================================================#
-lenconst = 30
-angleconst = 90
-
-#==============================================================#
 import turtle as tp
 
-#==============================================================#
+
+LEN: int = 30
+ANGLE: int = 90
+
 tp.left(90)
 tp.speed(0)
 
-#==============================================================#
 
-def onetree():
-    tp.forward(lenconst)
-    tp.backward(lenconst)
-    
-def tree(n):
+def main() -> None:
+    tree(7)
+
+
+def onetree() -> None:
+    tp.forward(LEN)
+    tp.backward(LEN)
+
+
+def tree(n: int=1) -> None:
     if n == 1:
         onetree()
-    else:
-        tp.forward(lenconst)
-        
-        reversetree(n - 1)
-        
-        tp.right(angleconst)
-        
-        tree(n - 1)
-        
-        tp.left(angleconst)
-        tp.backward(lenconst)
+        return
+    tp.forward(LEN)   
+    reversetree(n - 1)   
+    tp.right(ANGLE)   
+    tree(n - 1)  
+    tp.left(ANGLE)
+    tp.backward(LEN)
         
 
-def reversetree(n):
+def reversetree(n: int=1) -> None:
     if n == 1:
         onetree()
-    else:
-        tp.forward(lenconst)
-        
-        tree(n - 1)
-        
-        tp.left(angleconst)
-        
-        tree(n - 1)
-        
-        tp.right(angleconst)
-        tp.backward(lenconst)
+        return
+    tp.forward(LEN)
+    tree(n - 1)  
+    tp.left(ANGLE) 
+    tree(n - 1) 
+    tp.right(ANGLE)
+    tp.backward(LEN)
 
 
-def t(n):
+def t(n: int=1) -> int:
     if n == 1:
         return 1
-    else:
-        return 1 + (2 * (t(n - 1)))
+    return 1 + (2 * (t(n - 1)))
 
-def o(n):
-    #if n == 1:
-        #return 1
-    #else:
-        #return 2 * (o(n - 1))
+
+def o(n: int=1) -> int:
+    '''\
+    if n == 1:
+        return 1
+    return 2 * (o(n - 1))
+    '''
     
     return 2 ** (n - 1)
+    # 2 ** (1 - 1) == 1
 
-tree(7)
+
+main()
